43f16b8b612272aeb57901ef80218acb992d6573
SunPM <isunpm@qq.com>
2024-06-21 15:46:54 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <view flex="~ col items-center justify-center" p-50px>
+    <Logos />
+    <InputEntry />
+  </view>
+</template>
+
+<route lang="yaml" type="home">
+style:
+  navigationBarTitleText: 首页
+</route>
